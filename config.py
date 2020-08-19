8e4bca0873979b0cffb6ade1bdbf2760c1d0bc4b
relativemodder <61388196+relativemodder@users.noreply.github.com>
2020-08-20 01:21:43 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+#config goes here
+#здесь конфиг
+
+ACCESS_TOKEN="" #токен pushbullet
+
+userName=""
+password=""
+
+#0 - robtop
+#1 - gdps
+switcher = 0
+
+#server="http://servers.gdhost.pe.hu/HxTYn"
+server="http://www.boomlings.com/database"

b1c2b0ee8b49f37fa23dbe0907d2a9d62074bd30
relativemodder <61388196+relativemodder@users.noreply.github.com>
2020-08-20 21:30:05 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+#config goes here
+#здесь конфиг
+
+ACCESS_TOKEN="" #pushbullet token
+
+userName=""
+password=""
+
+#0 - robtop
+#1 - gdps
+switcher = 0
+
+#server="http://servers.gdhost.pe.hu/HxTYn"
+server="http://www.boomlings.com/database"
+
+mutelist = ['robtop', 'example']
